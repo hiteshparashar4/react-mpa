46409c1f8365b6302f32d0b87474bb2f2b272707
Hitesh Parashar <lycos_hit@yahoo.com>
2025-07-23 14:41:18 +0530

    add db secret

--- server/db.js
+++ server/db.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error('❌ MONGO_URI is not defined in environment variables.');
+}
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
